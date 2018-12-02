c0a817f50a38e13564d6f9b45f6fbb9c7352233f
Charles Nicholas Kellett <me@nicholaskellett.com>
2018-12-02 17:56:15 -0500

    Add fixture; simplify tests

---
+++ tests/DeploySoftware.LaunchPad.Shared.Tests/TfwWorldFileTestsFixture.cs
@@ -0,0 +1,56 @@
+﻿//LaunchPad Shared
+// Copyright (c) 2018 Deploy Software Solutions, inc. 
+
+#region license
+//Licensed under the Apache License, Version 2.0 (the "License"); 
+//you may not use this file except in compliance with the License. 
+//You may obtain a copy of the License at 
+
+//http://www.apache.org/licenses/LICENSE-2.0 
+
+//Unless required by applicable law or agreed to in writing, software 
+//distributed under the License is distributed on an "AS IS" BASIS, 
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+//See the License for the specific language governing permissions and 
+//limitations under the License. 
+#endregion
+
+using System.IO;
+using System.Reflection;
+using DeploySoftware.LaunchPad.Shared.Util;
+
+namespace DeploySoftware.LaunchPad.Shared.Tests
+{
+    using DeploySoftware.LaunchPad.Shared.Domain;
+    using System;
+
+    public class TfwWorldFileTestsFixture : IDisposable
+    {
+        public readonly TifWorldFileParser<Guid> TfwParser;
+
+        public readonly FileKey TfwFileKey;
+
+        public readonly TifWorldFile<Guid> TfwFile;
+
+        public TfwWorldFileTestsFixture()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            string fileName = Path.GetDirectoryName(path) + "\\" + "TifWorldFileTest.tfw";
+            TfwFileKey = new FileKey(fileName);
+            TfwParser = new TifWorldFileParser<Guid>();
+            TfwFile = TfwParser.GetTifWorldFileFromMetadataFile(TfwFileKey);
+        }
+
+        public void Initialize(FileKey radarsat1MetadataKey)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
--- tests/DeploySoftware.LaunchPad.Shared.Tests/TifWorldFileTests.cs
+++ tests/DeploySoftware.LaunchPad.Shared.Tests/TifWorldFileTests.cs
@@ -12,97 +12,57 @@ using Xunit;
 
 namespace DeploySoftware.LaunchPad.Shared.Tests
 {
-    public class TifWorldFileTests
+    public class TifWorldFileTests : IClassFixture<TfwWorldFileTestsFixture>
     {
+        
+        private readonly TfwWorldFileTestsFixture _fixture;
+
+        public TifWorldFileTests(TfwWorldFileTestsFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
         [Fact]
         public void FileExtension_Should_Be_DotTFW()
         {
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            string fileName = Path.GetDirectoryName(path) + "\\" + "TifWorldFileTest.tfw";
-            FileKey key = new FileKey(fileName);
-            TifWorldFileParser<Guid> parser = new TifWorldFileParser<Guid>();
-            TifWorldFile<Guid> file = parser.GetTifWorldFileFromMetadataFile(key);
-            file.FileExtension.Should().Be(".tfw");
+            
+            _fixture.TfwFile.FileExtension.Should().Be(".tfw");
         }
        
         [Fact]
         public void A_Should_Equal_First_Line_In_File()
         {
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            string fileName = Path.GetDirectoryName(path) + "\\" + "TifWorldFileTest.tfw";
-            FileKey key = new FileKey(fileName);
-            TifWorldFileParser<Guid> parser = new TifWorldFileParser<Guid>();
-            TifWorldFile<Guid> file = parser.GetTifWorldFileFromMetadataFile(key);
-            file.A.Should().Be(12.413247108000000m);
+            _fixture.TfwFile.A.Should().Be(12.413247108000000m);
         }
         
         [Fact]
         public void D_Should_Equal_Second_Line_In_File()
         {
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            string fileName = Path.GetDirectoryName(path) + "\\" + "TifWorldFileTest.tfw";
-            FileKey key = new FileKey(fileName);
-            TifWorldFileParser<Guid> parser = new TifWorldFileParser<Guid>();
-            TifWorldFile<Guid> file = parser.GetTifWorldFileFromMetadataFile(key);
-            file.D.Should().Be(0.000000000000000m);
+            _fixture.TfwFile.D.Should().Be(0.000000000000000m);
         }
 
         [Fact]
         public void B_Should_Equal_Third_Line_In_File()
         {
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            string fileName = Path.GetDirectoryName(path) + "\\" + "TifWorldFileTest.tfw";
-            FileKey key = new FileKey(fileName);
-            TifWorldFileParser<Guid> parser = new TifWorldFileParser<Guid>();
-            TifWorldFile<Guid> file = parser.GetTifWorldFileFromMetadataFile(key);
-            file.B.Should().Be(0.000000000000000m);
+            _fixture.TfwFile.B.Should().Be(0.000000000000000m);
         }
 
         [Fact]
         public void E_Should_Equal_Fourth_Line_In_File()
         {
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            string fileName = Path.GetDirectoryName(path) + "\\" + "TifWorldFileTest.tfw";
-            FileKey key = new FileKey(fileName);
-            TifWorldFileParser<Guid> parser = new TifWorldFileParser<Guid>();
-            TifWorldFile<Guid> file = parser.GetTifWorldFileFromMetadataFile(key);
-            file.E.Should().Be(-12.382885933000001m);
+            _fixture.TfwFile.E.Should().Be(-12.382885933000001m);
         }
         
         [Fact]
         public void C_Should_Equal_Fifth_Line_In_File()
         {
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            string fileName = Path.GetDirectoryName(path) + "\\" + "TifWorldFileTest.tfw";
-            FileKey key = new FileKey(fileName);
-            TifWorldFileParser<Guid> parser = new TifWorldFileParser<Guid>();
-            TifWorldFile<Guid> file = parser.GetTifWorldFileFromMetadataFile(key);
-            file.C.Should().Be(511283.0285078580m);
+            _fixture.TfwFile.C.Should().Be(511283.0285078580m);
         }
         
         [Fact]
         public void F_Should_Equal_Sixth_Line_In_File()
         {
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            string fileName = Path.GetDirectoryName(path) + "\\" + "TifWorldFileTest.tfw";
-            FileKey key = new FileKey(fileName);
-            TifWorldFileParser<Guid> parser = new TifWorldFileParser<Guid>();
-            TifWorldFile<Guid> file = parser.GetTifWorldFileFromMetadataFile(key);
-            file.F.Should().Be(7755841.0522885220m);
+            _fixture.TfwFile.F.Should().Be(7755841.0522885220m);
         }
         
     }
