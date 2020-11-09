25084132a3062d8951945fd32cb2401b113f50b9
Charles Nicholas Kellett <me@nicholaskellett.com>
2020-11-08 20:48:48 -0500

    Inherit from listresultsdto

--- src/DeploySoftware.LaunchPad.Core/Application/Dto/Get/GetAllDetailOutputDtoBase.cs
+++ src/DeploySoftware.LaunchPad.Core/Application/Dto/Get/GetAllDetailOutputDtoBase.cs
@@ -13,34 +13,7 @@ namespace DeploySoftware.LaunchPad.Core.Application
     public abstract partial class GetAllDetailOutputDtoBase<TIdType> : GetAllOutputDtoBase<TIdType>
     {
 
-        /// <summary>
-        /// The date and time that this object was created.
-        /// </summary>
-        [DataObjectField(false)]
-        [XmlAttribute]
-        public virtual DateTime CreationTime { get; set; }
-
-        /// <summary>
-        /// The user name that created the entity
-        /// </summary>
-        [DataObjectField(false)]
-        [XmlAttribute]
-        public virtual String CreatorUserName { get; set; }
-
-        /// <summary>
-        /// The date and time that the location and/or properties of this object were last modified.
-        /// </summary>
-        [DataObjectField(false)]
-        [XmlAttribute]
-        public virtual DateTime? LastModificationTime { get; set; }
-
-        /// <summary>
-        /// The user name that last modified the entity
-        /// </summary>
-        [DataObjectField(false)]
-        [XmlAttribute]
-        public virtual String LastModifierUserName { get; set; }
-
+       
         #region "Constructors"
 
         /// <summary>
@@ -48,213 +21,20 @@ namespace DeploySoftware.LaunchPad.Core.Application
         /// </summary>
         protected GetAllDetailOutputDtoBase() : base()
         {
-            Culture = ApplicationInformation<TIdType>.DEFAULT_CULTURE;
         }
 
-        /// <summary>
-        /// Default constructor where the id is known
-        /// </summary>
-        /// <param name="id"></param>
-        public GetAllDetailOutputDtoBase(int tenantId, TIdType id) : base()
-        {
-            TenantId = tenantId;
-            Id = id;
-            Culture = ApplicationInformation<TIdType>.DEFAULT_CULTURE;
-        }
-
-        public GetAllDetailOutputDtoBase(int tenantId, TIdType id, String culture) : base()
-        {
-            TenantId = tenantId;
-            Id = id;
-            Culture = culture;
-        }
 
         /// <summary>
         /// Serialization constructor used for deserialization
         /// </summary>
         /// <param name="info">The serialization info</param>
         /// <param name="context">The context of the stream</param>
-        protected GetAllDetailOutputDtoBase(SerializationInfo info, StreamingContext context)
+        protected GetAllDetailOutputDtoBase(SerializationInfo info, StreamingContext context) :base(info,context)
         {
-            Id = (TIdType)info.GetValue("Id", typeof(TIdType));
-            Culture = info.GetString("Culture");
-            Name = info.GetString("DisplayName");
-            TenantId = info.GetInt32("TenantId");
-            CreationTime = info.GetDateTime("CreationTime");
-            CreatorUserName = info.GetString("CreatorUserName");
-            LastModifierUserName = info.GetString("LastModifierUserName");
-            LastModificationTime = info.GetDateTime("LastModificationTime");
+
         }
 
         #endregion
-
-        /// <summary>
-        /// The method required for implementing ISerializable
-        /// </summary>
-        /// <param name="info"></param>
-        /// <param name="context"></param>
-        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue("TenantId", TenantId);
-            info.AddValue("Id", Id);
-            info.AddValue("Culture", Culture);
-            info.AddValue("Name", Name);
-            info.AddValue("CreationTime", CreationTime);
-            info.AddValue("CreatorUserName", CreatorUserName);
-            info.AddValue("LastModifierUserName", LastModifierUserName);
-            info.AddValue("LastModificationTime", LastModificationTime);
-        }
-
-        /// <summary>  
-        /// Displays information about the class in readable format.  
-        /// </summary>  
-        /// <returns>A string representation of the object.</returns>
-        public override string ToString()
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.Append("[GetAllDetailOutputDtoBase : ");
-            sb.Append(ToStringBaseProperties());
-            sb.Append("]");
-            return sb.ToString();
-        }
-
-        /// <summary>
-        /// This method makes it easy for any child class to generate a ToString() representation of
-        /// the common base properties
-        /// </summary>
-        /// <returns>A string description of the entity</returns>
-        protected override String ToStringBaseProperties()
-        {
-            StringBuilder sb = new StringBuilder();
-            // LaunchPAD RAD properties
-            sb.AppendFormat("Id={0};", Id);
-            sb.AppendFormat("Culture={0};", Culture);
-            sb.AppendFormat("Name={0};", Name);
-            sb.AppendFormat("CreatorUserName={0};", CreatorUserName);
-            sb.AppendFormat("LastModifierUserName={0};", LastModifierUserName);
-
-            // ABP properties
-            //
-            sb.AppendFormat("CreationTime={0};", CreationTime);
-            sb.AppendFormat("LastModificationTime={0};", LastModificationTime);
-            sb.AppendFormat("TenantId={0};", TenantId);
-
-            return sb.ToString();
-        }
-
-        /// <summary>
-        /// Shallow clones the entity
-        /// </summary>
-        /// <typeparam name="TEntity">The source entity to clone</typeparam>
-        /// <returns>A shallow clone of the entity and its serializable properties</returns>
-        protected new TEntity Clone<TEntity>() where TEntity : GetAllDetailOutputDtoBase<TIdType>, new()
-        {
-            TEntity clone = new TEntity();
-            foreach (PropertyInfo info in GetType().GetProperties())
-            {
-                // ensure the property type is serializable
-                if (info.GetType().IsSerializable)
-                {
-                    PropertyInfo cloneInfo = GetType().GetProperty(info.Name);
-                    cloneInfo.SetValue(clone, info.GetValue(this, null), null);
-                }
-            }
-            return clone;
-        }
-
-        /// <summary>
-        /// Comparison method between two objects of the same type, used for sorting.
-        /// Because the CompareTo method is strongly typed by generic constraints,
-        /// it is not necessary to test for the correct object type.
-        /// </summary>
-        /// <param name="other">The other object of this type we are comparing to</param>
-        /// <returns></returns>
-        public virtual int CompareTo(GetAllDetailOutputDtoBase<TIdType> other)
-        {
-            // put comparison of properties in here 
-            // for base object we'll just sort by name and description short
-            return Name.CompareTo(other.Name);
-        }
-
-        /// <summary>
-        /// Override the legacy Equals. Must cast obj in this case.
-        /// </summary>
-        /// <param name="obj">A type to check equivalency of (hopefully) an Entity</param>
-        /// <returns>True if the entities are the same according to business key value</returns>
-        public override bool Equals(object obj)
-        {
-            if (obj != null && obj is GetAllDetailOutputDtoBase<TIdType>)
-            {
-                return Equals(obj as GetAllDetailOutputDtoBase<TIdType>);
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Equality method between two objects of the same type.
-        /// Because the Equals method is strongly typed by generic constraints,
-        /// it is not necessary to test for the correct object type.
-        /// For safety we just want to match on business key value - in this case the fields
-        /// that cannot be different between the two objects if they are supposedly equal.        
-        /// </summary>
-        /// <param name="obj">The other object of this type that we are testing equality with</param>
-        /// <returns></returns>
-        public virtual bool Equals(GetAllDetailOutputDtoBase<TIdType> obj)
-        {
-            if (obj != null)
-            {
-                return Id.Equals(obj.Id) && Culture.Equals(obj.Culture) && TenantId.Equals(obj.TenantId)
-                    && CreationTime.Equals(obj.CreationTime)
-                    && CreatorUserName.Equals(obj.CreatorUserName)
-                    && LastModifierUserName.Equals(obj.LastModifierUserName)
-                    && LastModificationTime.Equals(obj.LastModificationTime)
-                ;
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Override the == operator to test for equality
-        /// </summary>
-        /// <param name="x">The first value</param>
-        /// <param name="y">The second value</param>
-        /// <returns>True if both objects are fully equal based on the Equals logic</returns>
-        public static bool operator ==(GetAllDetailOutputDtoBase<TIdType> x, GetAllDetailOutputDtoBase<TIdType> y)
-        {
-            if (x is null)
-            {
-                if (y is null)
-                {
-                    return true;
-                }
-                return false;
-            }
-            return x.Equals(y);
-        }
-
-        /// <summary>
-        /// Override the != operator to test for inequality
-        /// </summary>
-        /// <param name="x">The first value</param>
-        /// <param name="y">The second value</param>
-        /// <returns>True if both objects are not equal based on the Equals logic</returns>
-        public static bool operator !=(GetAllDetailOutputDtoBase<TIdType> x, GetAllDetailOutputDtoBase<TIdType> y)
-        {
-            return !(x == y);
-        }
-
-        /// <summary>  
-        /// Computes and retrieves a hash code for an object.  
-        /// </summary>  
-        /// <remarks>  
-        /// This method implements the <see cref="Object">Object</see> method.  
-        /// </remarks>  
-        /// <returns>A hash code for an object.</returns>
-        public override int GetHashCode()
-        {
-            return Id.GetHashCode() + Culture.GetHashCode() + TenantId.GetHashCode();
-        }
 
     }
 }
