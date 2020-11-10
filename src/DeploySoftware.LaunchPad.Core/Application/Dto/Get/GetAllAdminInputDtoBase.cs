e11c505edebe1acf7779a9097d3fc376e11fc78e
Charles Nicholas Kellett <me@nicholaskellett.com>
2020-11-09 21:47:35 -0500

    Base DTO for GetAllAdmin method

---
+++ src/DeploySoftware.LaunchPad.Core/Application/Dto/Get/GetAllAdminInputDtoBase.cs
@@ -0,0 +1,80 @@
+﻿
+using DeploySoftware.LaunchPad.Core.Domain;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace DeploySoftware.LaunchPad.Core.Application
+{
+    public abstract partial class GetAllAdminInputDtoBase<TIdType> : GetAllDetailInputDtoBase<TIdType>
+    {
+        /// <summary>
+        /// If this object is not a translation this field will be null. 
+        /// If this object is a translation, this id references the parent object.
+        /// </summary>
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public virtual TIdType TranslatedFromId { get; set; }
+
+        /// <summary>
+        /// The date and time that this object was deleted.
+        /// </summary>
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public virtual DateTime? DeletionTime { get; set; }
+
+        /// <summary>
+        /// The id of the user which deleted this entity
+        /// </summary>
+        [DataObjectField(false)]
+        [XmlAttribute]
+        [ForeignKey(nameof(DeleterUserId))]
+        public virtual long? DeleterUserId { get; set; }
+
+
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public virtual bool IsDeleted { get; set; }
+
+        #region "Constructors"
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        protected GetAllAdminInputDtoBase() : base()
+        {
+            Culture = ApplicationInformation<TIdType>.DEFAULT_CULTURE;
+        }
+
+        /// <summary>
+        /// Default constructor where the id is known
+        /// </summary>
+        /// <param name="id"></param>
+        public GetAllAdminInputDtoBase(int tenantId) : base()
+        {
+            TenantId = tenantId;
+            Culture = ApplicationInformation<TIdType>.DEFAULT_CULTURE;
+        }
+
+        public GetAllAdminInputDtoBase(int tenantId, String culture) : base()
+        {
+            TenantId = tenantId;
+            Culture = culture;
+        }
+
+        /// <summary>
+        /// Serialization constructor used for deserialization
+        /// </summary>
+        /// <param name="info">The serialization info</param>
+        /// <param name="context">The context of the stream</param>
+        protected GetAllAdminInputDtoBase(SerializationInfo info, StreamingContext context) : base(info,context)
+        {
+            
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/DeploySoftware.LaunchPad.Core/Application/Dto/Get/GetAllAdminOutputDtoBase.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace DeploySoftware.LaunchPad.Core.Application
+{
+    public abstract partial class GetAllAdminOutputDtoBase<TIdType> : GetAllDetailOutputDtoBase<TIdType>
+    {
+        
+        #region "Constructors"
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        protected GetAllAdminOutputDtoBase() : base()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Serialization constructor used for deserialization
+        /// </summary>
+        /// <param name="info">The serialization info</param>
+        /// <param name="context">The context of the stream</param>
+        protected GetAllAdminOutputDtoBase(SerializationInfo info, StreamingContext context) :base(info,context)
+        {
+
+        }
+
+        #endregion
+
+    }
+}
