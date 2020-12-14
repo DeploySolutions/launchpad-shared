6c58d8ef412e4298f2ad0edac42c378849b295ce
Charles Nicholas Kellett <me@nicholaskellett.com>
2020-12-13 20:13:11 -0500

    Simplify util folder structure; refactor inheritance for generated objects

---
+++ src/DeploySoftware.LaunchPad.Core/Domain/Entities/SoftwareApplications/LaunchPadGeneratedObjectBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeploySoftware.LaunchPad.Core.Domain.Entities.SoftwareApplications
+{
+    /// <summary>
+    /// The base class containing properties used for LaunchPad RAD code generation processes.
+    /// </summary>
+    public abstract partial class LaunchPadGeneratedObjectBase
+    {
+        /// <summary>
+        /// The singular name of the object 
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The generated object name, in the plural form
+        /// </summary>
+        public string NamePlural { get; set; }
+
+        /// <summary>
+        /// The description of the object
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Code annotations for the object
+        /// </summary>
+        public string Annotations { get; set; }
+    }
+}
