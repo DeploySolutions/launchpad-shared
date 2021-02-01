4a691e7b9559a420d837482dfaa7be221ba0773d
Charles Nicholas Kellett <me@nicholaskellett.com>
2021-01-31 21:27:25 -0500

    Add launchpadgenerated folder

---
+++ src/DeploySoftware.LaunchPad.Core/FileGeneration/Documents/LaunchPadGeneratedFolder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeploySoftware.LaunchPad.Core.FileGeneration
+{
+    public partial class LaunchPadGeneratedFolder : LaunchPadGeneratedObjectBase
+    {
+        public IList<LaunchPadGeneratedFolder> SubFolders { get; set; }
+
+        public LaunchPadGeneratedFolder() : base()
+        {
+            SubFolders = new List<LaunchPadGeneratedFolder>();
+        }
+    }
+}
