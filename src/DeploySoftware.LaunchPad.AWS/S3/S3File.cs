276f836f0cc329a69380017b6438162b7b295cb6
Charles Nicholas Kellett <me@nicholaskellett.com>
2022-02-12 21:51:56 -0500

    Create AWS folders for services and reorg classes

---
+++ src/DeploySoftware.LaunchPad.AWS/S3/S3File.cs
@@ -0,0 +1,58 @@
+﻿using DeploySoftware.LaunchPad.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DeploySoftware.LaunchPad.AWS
+{
+    public partial class S3File<TIdType, TFileContentType> : FileBase<TIdType, TFileContentType, S3BucketStorageLocation>
+        where TFileContentType: class
+    {
+        /// <summary>
+        /// The full path of the file
+        /// </summary>
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public virtual String ObjectKey
+        {
+            get
+            {
+                return Location.Prefix + "/" + Name;
+            }
+        }
+
+        /// <summary>
+        /// The full path of the file
+        /// </summary>
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public virtual Uri VirtualPathUri
+        {
+            get
+            {
+                return new Uri("https://" + Location.BucketName + ".s3.amazonaws.com/" + Location.Prefix + "/" + Name.Replace(" ","+"));
+            }
+        }
+
+        /// <summary>
+        /// The full path of the file
+        /// </summary>
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public override Uri FullPathUri
+        {
+            get
+            {
+                return new Uri("https://s3." + Location.Region + ".amazonaws.com/" + Location.BucketName + "/" + Location.Prefix + "/" + Name.Replace(" ", "+"));
+            }
+        }
+
+        public S3File() : base()
+        {
+
+        }
+
+    }
+}
