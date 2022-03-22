848d430fd77838b4ccab8eb94173a580f87e8f06
Charles Nicholas Kellett <me@nicholaskellett.com>
2022-03-22 09:48:58 -0400

    Fix typo in classname

--- src/DeploySoftware.LaunchPad.Space.Satellites/GoC/Radarsat2Observation.cs
+++ src/DeploySoftware.LaunchPad.Space.Satellites/GoC/Radarsat2Observation.cs
@@ -30,7 +30,7 @@ namespace DeploySoftware.LaunchPad.Space.Satellites.GoC
     using DeploySoftware.LaunchPad.Core.Domain;
 
     [Serializable()]
-    public partial class RadarsatO2bservation<TPrimaryKey, TFileStorageLocationType> : EarthObservationBase<TPrimaryKey, TFileStorageLocationType>,
+    public partial class Radarsat2Observation<TPrimaryKey, TFileStorageLocationType> : EarthObservationBase<TPrimaryKey, TFileStorageLocationType>,
         IRadarsatObservation<TPrimaryKey, TFileStorageLocationType>
         where TFileStorageLocationType : IFileStorageLocation, new()
     {
@@ -49,12 +49,12 @@ namespace DeploySoftware.LaunchPad.Space.Satellites.GoC
         [JsonProperty("title")]
         public string Title { get; set; }
 
-        protected RadarsatO2bservation() : base()
+        protected Radarsat2Observation() : base()
         {
             CurrentLocation = new SpaceTimeInformation();
         }
 
-        public RadarsatO2bservation(string title) : base()
+        public Radarsat2Observation(string title) : base()
         {
             Title = title;
             CurrentLocation = new SpaceTimeInformation();
