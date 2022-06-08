7460cb60d2d043ffbff2562ac270119d92ecc069
Charles Nicholas Kellett <me@nicholaskellett.com>
2022-06-08 12:06:31 -0400

    Update constructors

--- src/DeploySoftware.LaunchPad.Core/Application/SystemIntegrationServiceBase.cs
+++ src/DeploySoftware.LaunchPad.Core/Application/SystemIntegrationServiceBase.cs
@@ -1,5 +1,7 @@
 ﻿using Abp;
 using Castle.Core.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +13,26 @@ namespace DeploySoftware.LaunchPad.Core.Application
     [Serializable()]
     public abstract class SystemIntegrationServiceBase : AbpServiceBase, ISystemIntegrationService
     {
+        protected readonly IConfigurationRoot _configurationRoot;
+        public IConfigurationRoot ConfigurationRoot { get { return _configurationRoot; } }
+
+
         public new ILogger Logger { get; set; }
 
-        protected SystemIntegrationServiceBase() { }
+        public SystemIntegrationServiceBase() 
+        {
+            Logger = NullLogger.Instance;
+        }
 
         public SystemIntegrationServiceBase(ILogger logger)
         {
             Logger = logger;
+        }
+
+        public SystemIntegrationServiceBase(ILogger logger, IConfigurationRoot configurationRoot)
+        {
+            Logger = logger;
+            _configurationRoot = configurationRoot;
         }
 
     }
