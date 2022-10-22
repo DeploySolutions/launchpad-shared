3a73bfbe7a7e81116d2794c809b548c86cb71d9c
Charles Nicholas Kellett <me@nicholaskellett.com>
2022-10-22 15:01:49 -0400

    Add LaunchPad Configuration Accessor for dependency injection

---
+++ src/DeploySoftware.LaunchPad.Core.Abp/AbpModuleConfig/LaunchPadAbpAppConfigurationAccessor.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Dependency;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace DeploySoftware.LaunchPad.Core.Abp.AbpModuleConfig
+{
+    public class LaunchPadAbpAppConfigurationAccessor : ILaunchPadAppConfigurationAccessor, ISingletonDependency
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public LaunchPadAbpAppConfigurationAccessor(IWebHostEnvironment env)
+        {
+            Configuration = env.GetAppConfiguration();
+        }
+    }
+}
