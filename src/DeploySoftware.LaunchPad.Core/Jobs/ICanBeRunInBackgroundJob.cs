cd13c06781821e495932a056e89b19b5670eb2dc
Charles Nicholas Kellett <me@nicholaskellett.com>
2022-10-22 13:32:10 -0400

    Add background job marker interface

---
+++ src/DeploySoftware.LaunchPad.Core/Jobs/ICanBeRunInBackgroundJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeploySoftware.LaunchPad.Core.Jobs
+{
+    public interface ICanBeRunInBackgroundJob
+    {
+        public void Run();
+
+    }
+}
