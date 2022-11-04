6c9ca26e7f0153b095597c5d19fb38c643b5fd9f
Charles Nicholas Kellett <me@nicholaskellett.com>
2022-11-04 08:57:20 -0400

    Add AwsClientSettings object

---
+++ src/DeploySoftware.LaunchPad.AWS/AwsClientSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeploySoftware.LaunchPad.AWS
+{
+    [Serializable]
+    public partial class AwsClientSettings : IAwsClientSettings
+    {
+        public virtual int RequestTimeoutInMs { get; set; } = 10000;
+        public virtual int SocketReadWriteTimeoutInMs { get; set; } = 30000;
+        public virtual int MaxErrorRetry { get; set; } = 2;
+
+        public AwsClientSettings()
+        {
+
+        }
+    }
+}
---
+++ src/DeploySoftware.LaunchPad.AWS/IAwsClientSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeploySoftware.LaunchPad.AWS
+{
+    public interface IAwsClientSettings
+    {
+        public int RequestTimeoutInMs { get; set; }
+
+        public int SocketReadWriteTimeoutInMs { get; set; }
+
+        public int MaxErrorRetry { get; set; }
+
+    }
+}
