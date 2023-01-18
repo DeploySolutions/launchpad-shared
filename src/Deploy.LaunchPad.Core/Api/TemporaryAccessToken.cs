eafb33fccdcc3dbc42a8842458ea667d244b68a4
Charles Nicholas Kellett <me@nicholaskellett.com>
2023-01-18 08:01:58 -0500

    Add json property naming for bearer tokens

--- src/Deploy.LaunchPad.Core/Api/TemporaryAccessToken.cs
+++ src/Deploy.LaunchPad.Core/Api/TemporaryAccessToken.cs
@@ -1,16 +1,24 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace Deploy.LaunchPad.Core.Api
 {
     public class TemporaryAccessToken
     {
+        [JsonPropertyName("access_token")]
         public string AccessToken { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; } = "Bearer";
 
         public int ExpiresIn { get; set; }
 
         public DateTime ExpiryDateTime { get; set; }
 
-        public string TokenType { get; set; } = "Bearer";
+        public TemporaryAccessToken()
+        {
+
+        }
 
     }
 }
