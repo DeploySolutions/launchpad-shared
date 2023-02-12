a516c87d8138d68ffb9a1446bed772a27bd64471
Charles Nicholas Kellett <me@nicholaskellett.com>
2023-02-12 13:07:32 -0500

    Create PythonListJsonConverter

---
+++ src/Deploy.LaunchPad.Python/Json/PythonInstallationJson.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Deploy.LaunchPad.Python.Json
+{
+    [Serializable()]
+    public partial class PythonInstallationJson
+    {
+        public virtual string Id { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual string DescriptionShort { get; set; }
+
+        public virtual string? DescriptionFull { get; set; }
+        
+        public virtual PythonVersionJson Version { get; set; }
+
+        public virtual Uri InstallLocation { get; set; }
+
+        public virtual IDictionary<string, Uri> ModuleLocations { get; set; }
+
+        public PythonInstallationJson() : base()
+        {
+            Version = new PythonVersionJson(3, 11, 2);
+            var comparer = StringComparer.OrdinalIgnoreCase;
+            ModuleLocations = new Dictionary<string, Uri>(comparer);
+            Id = "py" + Version.ToString();
+            Name = "Python " + Version.ToString();
+            DescriptionShort = Name;
+            DescriptionFull = DescriptionShort;
+        }
+    }
+
+    [Serializable()]
+    public partial class PythonVersionJson
+    {
+        public virtual int Major { get; private set; }
+
+        [Required]
+        [DataObjectField(false)]
+        [XmlElement]
+        public virtual int Minor { get; private set; }
+
+        [DataObjectField(false)]
+        [XmlElement]
+        public virtual int Patch { get; set; } = 0;
+        public PythonVersionJson()
+        {
+
+        }
+
+        public PythonVersionJson(int majorVersion, int minorVersion, int patchVersion)
+        {
+            Major = majorVersion;
+            Minor = minorVersion;
+            Patch = patchVersion;
+        }
+    }
+}
---
+++ src/Deploy.LaunchPad.Python/Json/PythonListJsonConverter.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Deploy.LaunchPad.Python.Json;
+
+namespace Deploy.LaunchPad.Python
+{
+    public partial class PythonListJsonConverter : JsonConverter<List<PythonInstallationJson>>
+    {
+        public override List<PythonInstallationJson> ReadJson(JsonReader reader, Type objectType, List<PythonInstallationJson> existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            var token = JToken.Load(reader);
+            var list = Activator.CreateInstance(objectType) as List<PythonInstallationJson>;
+            if (token.HasValues)
+            {
+                var childValues = token.Values();
+                JTokenType type = childValues.FirstOrDefault().Type;
+                if (type != JTokenType.Null && childValues.First().Any())
+                {
+                    if (type == JTokenType.Object) // it's a single object
+                    {
+                        var installationToken = childValues.FirstOrDefault();
+                        PythonInstallationJson newObject = installationToken.ToObject<PythonInstallationJson>();
+                        list.Add(newObject);
+                    }
+                    if (type == JTokenType.Array) // it's an array of objects
+                    {
+                        foreach (var child in childValues)
+                        {
+                            var locationTokenArray = child.Children();
+                            foreach (var locationToken in locationTokenArray)
+                            {
+                                PythonInstallationJson newObject = locationToken.ToObject<PythonInstallationJson>();
+                                list.Add(newObject);
+                            }
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+
+        public override void WriteJson(JsonWriter writer, List<PythonInstallationJson> value, JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
+    }
+
+    
+}
