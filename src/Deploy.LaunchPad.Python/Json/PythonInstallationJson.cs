8d6b912e44cf7bd71829856da2c46f17439d12e1
Charles Nicholas Kellett <me@nicholaskellett.com>
2023-02-14 23:06:38 -0500

    Move class into its own file

--- src/Deploy.LaunchPad.Python/Json/PythonInstallationJson.cs
+++ src/Deploy.LaunchPad.Python/Json/PythonInstallationJson.cs
@@ -32,29 +32,4 @@ namespace Deploy.LaunchPad.Python.Json
         }
     }
 
-    [Serializable()]
-    public partial class PythonVersionJson
-    {
-        public virtual int Major { get; private set; }
-
-        [Required]
-        [DataObjectField(false)]
-        [XmlElement]
-        public virtual int Minor { get; private set; }
-
-        [DataObjectField(false)]
-        [XmlElement]
-        public virtual int Patch { get; set; } = 0;
-        public PythonVersionJson()
-        {
-
-        }
-
-        public PythonVersionJson(int majorVersion, int minorVersion, int patchVersion)
-        {
-            Major = majorVersion;
-            Minor = minorVersion;
-            Patch = patchVersion;
-        }
-    }
 }
---
+++ src/Deploy.LaunchPad.Python/Json/PythonVersionJson.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Deploy.LaunchPad.Python.Json
+{
+    
+    [Serializable()]
+    public partial class PythonVersionJson
+    {
+        public virtual int Major { get; private set; }
+
+        [Required]
+        [DataObjectField(false)]
+        [XmlElement]
+        public virtual int Minor { get; private set; }
+
+        [DataObjectField(false)]
+        [XmlElement]
+        public virtual int Patch { get; set; } = 0;
+        public PythonVersionJson()
+        {
+
+        }
+
+        public PythonVersionJson(int majorVersion, int minorVersion, int patchVersion)
+        {
+            Major = majorVersion;
+            Minor = minorVersion;
+            Patch = patchVersion;
+        }
+    }
+}
