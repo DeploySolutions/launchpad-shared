fce80f8d8a41c1df68d85d2d4e839110ed6e575c
Charles Nicholas Kellett <me@nicholaskellett.com>
2023-05-13 13:43:11 -0400

    Upgrade packages

---
+++ src/Deploy.LaunchPad.Core/Domain/Geography/IObservationPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core.Domain.Geography
+{
+    public partial interface IObservationPoint<TPrimaryKey> : ILaunchPadObject
+    {
+        public IAreaOfInterest<TPrimaryKey> ParentAoi { get; set; }
+
+        public IGeographicLocation Location { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
