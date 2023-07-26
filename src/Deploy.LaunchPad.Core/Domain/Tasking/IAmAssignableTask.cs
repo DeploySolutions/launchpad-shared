567b3c4f949d5430e8bd9ab2bd6fa8639fe44bc6
Charles Nicholas Kellett <11150209+NickKellett@users.noreply.github.com>
2023-07-26 13:36:18 -0400

    Add basic tasking interfaces

---
+++ src/Deploy.LaunchPad.Core/Domain/Tasking/IAmAssignableTask.cs
@@ -0,0 +1,24 @@
+﻿using Deploy.LaunchPad.Core.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core.Domain.Tasking
+{
+    public partial interface IAmAssignableTask<TIdType> : ILaunchPadDomainEntityProperties<TIdType>
+    {        
+
+        public string InstructionsShort { get; set; }
+        public string InstructionsDetailed { get; set; }
+
+        public Uri MoreInformationUri { get; set; }
+
+        public IDictionary<string, IAmAssignableTask<TIdType>> DependentOn { get; set; }
+
+        public IDictionary<string, IAmAssignableTask<TIdType>> SubTasks { get; set; }
+
+        public string Priority { get; set; }
+    }
+}
---
+++ src/Deploy.LaunchPad.Core/Domain/Tasking/IAmAssignedTaskResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core.Domain.Tasking
+{
+    /// <summary>
+    /// Once an assigned task is responded to by the assignee
+    /// </summary>
+    public partial interface IAmAssignedTaskResponse<TTdType> : IAmAssignedTaskRequest<TTdType>
+    {
+        public string Progress { get; set; }
+
+        public DateTime? CompletedDate { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
