752a6fe78016d613f67cb9097fbdf8041fc4d47c
Charles Nicholas Kellett <me@nicholaskellett.com>
2023-10-15 21:44:44 -0400

    Add optional json files parameter

--- src/Deploy.LaunchPad.Core.Abp/AbpModuleConfig/LaunchPadHostingEnvironmentExtensions.cs
+++ src/Deploy.LaunchPad.Core.Abp/AbpModuleConfig/LaunchPadHostingEnvironmentExtensions.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Deploy.LaunchPad.Core.Abp.AbpModuleConfig
 {
     public static class LaunchPadHostingEnvironmentExtensions
     {
-        public static IConfigurationRoot GetAppConfiguration(this IWebHostEnvironment env, string userSecretId = "")
+        public static IConfigurationRoot GetAppConfiguration(this IWebHostEnvironment env, string userSecretId = "", IList<string> jsonFiles = null)
         {
-            return LaunchPadAbpAppConfigurations.Get(env.ContentRootPath, env.EnvironmentName, env.IsDevelopment(), userSecretId);
+            return LaunchPadAbpAppConfigurations.Get(env.ContentRootPath, env.EnvironmentName, env.IsDevelopment(), userSecretId, jsonFiles);
         }
     }
 }
