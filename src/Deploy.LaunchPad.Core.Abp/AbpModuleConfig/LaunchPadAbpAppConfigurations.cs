745dca24ffb269419fac6f66340630dd21fad068
Charles Nicholas Kellett <me@nicholaskellett.com>
2023-10-15 21:33:34 -0400

    Add optional files

--- src/Deploy.LaunchPad.Core.Abp/AbpModuleConfig/LaunchPadAbpAppConfigurations.cs
+++ src/Deploy.LaunchPad.Core.Abp/AbpModuleConfig/LaunchPadAbpAppConfigurations.cs
@@ -1,6 +1,8 @@
 ﻿using Abp.Extensions;
 using Microsoft.Extensions.Configuration;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Deploy.LaunchPad.Core.Abp.AbpModuleConfig
 {
@@ -13,16 +15,16 @@ namespace Deploy.LaunchPad.Core.Abp.AbpModuleConfig
             _configurationCache = new ConcurrentDictionary<string, IConfigurationRoot>();
         }
 
-        public static IConfigurationRoot Get(string path, string environmentName = null, bool addUserSecrets = false, string userSecretId = "")
+        public static IConfigurationRoot Get(string path, string environmentName = null, bool addUserSecrets = false, string userSecretId = "", IList<string> jsonFiles = null)
         {
             var cacheKey = path + "#" + environmentName + "#" + addUserSecrets;
             return _configurationCache.GetOrAdd(
                 cacheKey,
-                _ => BuildConfiguration(path, environmentName, addUserSecrets, userSecretId)
+                _ => BuildConfiguration(path, environmentName, addUserSecrets, userSecretId, jsonFiles)
             );
         }
 
-        private static IConfigurationRoot BuildConfiguration(string path, string environmentName = null, bool addUserSecrets = false, string userSecretId = "")
+        private static IConfigurationRoot BuildConfiguration(string path, string environmentName = null, bool addUserSecrets = false, string userSecretId = "", IList<string> jsonFiles = null)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(path)
@@ -34,10 +36,20 @@ namespace Deploy.LaunchPad.Core.Abp.AbpModuleConfig
             }
 
             builder = builder.AddEnvironmentVariables();
-
             if (addUserSecrets && !string.IsNullOrEmpty(userSecretId))
             {
                 builder.AddUserSecrets(userSecretId);
+            }
+
+            if (jsonFiles != null)
+            { 
+                foreach( var jsonFile in jsonFiles)
+                {
+                    builder.AddJsonFile(jsonFile,
+                        optional: false,
+                        reloadOnChange: true
+                    );
+                }
             }
 
             return builder.Build();
