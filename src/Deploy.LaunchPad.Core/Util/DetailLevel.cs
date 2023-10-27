87b77fca5f1db85582cf80cbb323019f7f6c53ef
Charles Nicholas Kellett <me@nicholaskellett.com>
2023-10-27 13:23:53 -0400

    Add detail level enum

---
+++ src/Deploy.LaunchPad.Core/Util/DetailLevel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core.Util
+{
+    /// <summary>
+    /// Useful for specifying the level of detail required for a method's return
+    /// </summary>
+    public enum DetailLevel
+    {
+        Basic = 0,
+        Detailed = 1,
+        Full = 2,
+        Admin = 3
+    }
+}
