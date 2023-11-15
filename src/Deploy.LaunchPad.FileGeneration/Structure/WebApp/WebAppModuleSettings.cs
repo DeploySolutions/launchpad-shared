ea98218301d038b990bf3ea3068cdffac85ee5a2
Charles Nicholas Kellett <me@nicholaskellett.com>
2023-11-15 09:33:50 -0500

    Add base namespace element

--- src/Deploy.LaunchPad.FileGeneration/Structure/WebApp/WebAppModuleSettings.cs
+++ src/Deploy.LaunchPad.FileGeneration/Structure/WebApp/WebAppModuleSettings.cs
@@ -10,14 +10,18 @@ namespace Deploy.LaunchPad.FileGeneration.Structure
         /// <summary>
         /// The name of the web app
         /// </summary>
-        public virtual string WebAppName { get; set; }
+        public virtual string WebAppName { get; set; } = string.Empty;
 
         /// The name of the Visual Studio solution (.sln) in which this generated module will belong.
         /// Note: this solution configuration is deliberately placed at the Visual Studio Module level, 
         /// and is not the same as a LaunchPadGeneratedSolution object.
         /// </summary>
-        public virtual string VisualStudioSolutionName { get; set; }
+        public virtual string VisualStudioSolutionName { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The base namespace of the generated items in all the projects.
+        /// </summary>
+        public virtual string BaseNamespace { get; set; } = string.Empty;
 
         /// <summary>
         /// Contains the appsettings JSON elements belonging to this component
@@ -26,8 +30,6 @@ namespace Deploy.LaunchPad.FileGeneration.Structure
 
         public WebAppModuleSettings() : base()
         {
-            WebAppName = string.Empty;
-            VisualStudioSolutionName = string.Empty;
         }
     }
 }
