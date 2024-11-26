3aa70301ef2b3b580b372319945c6ce47274af66
Charles Nicholas Kellett <11150209+NickKellett@users.noreply.github.com>
2024-11-25 21:36:18 -0500

    Add custom sorting to class representation properties and methods

--- src/Deploy.LaunchPad.Core/Classes/ClassRepresentation.cs
+++ src/Deploy.LaunchPad.Core/Classes/ClassRepresentation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Deploy.LaunchPad.Core.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,72 @@ namespace Deploy.LaunchPad.Core.Classes
     {
         public virtual ElementName Name { get; set; }
         public virtual ElementDescription Description { get; set; }
-        public virtual Dictionary<string, PropertyRepresentation> Properties { get; set; }
-        public virtual Dictionary<string, MethodRepresentation> Methods { get; set; }
+        public virtual SortedDictionary<string, PropertyRepresentation> Properties { get; set; } = new SortedDictionary<string, PropertyRepresentation>();
+
+        public virtual List<string> PropertySortOrder { get; set; } = new List<string>();
+
+        public virtual SortedDictionary<string, MethodRepresentation> Methods { get; set; } = new SortedDictionary<string, MethodRepresentation>();
+
+        public virtual List<string> MethodSortOrder { get; set; } = new List<string>();
+        
 
         public ClassRepresentation(string name)
         {
             Name = new ElementName(name);
             Description = new ElementDescription(name);
-            Properties = new Dictionary<string, PropertyRepresentation>();
-            Methods = new Dictionary<string, MethodRepresentation>();
         }
+
+
+        /// <summary>
+        /// Set the PropertySortOrder with the provided list and then 
+        /// sort the properties according to the PropertySortOrder. 
+        /// 
+        /// </summary>
+        /// <param name="propertySortOrder"></param>
+        public virtual void SortMethods(List<string> methodSortOrder)
+        {
+            Guard.Against<ArgumentNullException>(methodSortOrder == null, "methodSortOrder cannot be null.");
+            MethodSortOrder = methodSortOrder;
+            SortMethods();
+        }
+
+        /// <summary>
+        /// Sort the properties according to the PropertySortOrder (if any is set, if not then the existing order will not change). 
+        /// 
+        /// </summary>
+        public virtual void SortMethods()
+        {
+            Guard.Against<ArgumentNullException>(MethodSortOrder == null, "MethodSortOrder cannot be null.");
+            Methods = new SortedDictionary<string, MethodRepresentation>(
+               Methods,
+               new LaunchPadCustomPropertyOrderComparer(MethodSortOrder)
+            );
+        }
+        /// <summary>
+        /// Set the PropertySortOrder with the provided list and then 
+        /// sort the properties according to the PropertySortOrder. 
+        /// 
+        /// </summary>
+        /// <param name="propertySortOrder"></param>
+        public virtual void SortProperties(List<string> propertySortOrder)
+        {
+            Guard.Against<ArgumentNullException>(propertySortOrder == null, "propertySortOrder cannot be null.");
+            PropertySortOrder = propertySortOrder;
+            SortProperties();
+        }
+
+        /// <summary>
+        /// Sort the properties according to the PropertySortOrder (if any is set, if not then the existing order will not change). 
+        /// 
+        /// </summary>
+        public virtual void SortProperties()
+        {
+            Guard.Against<ArgumentNullException>(PropertySortOrder == null, "PropertySortOrder cannot be null.");
+            Properties = new SortedDictionary<string, PropertyRepresentation>(
+               Properties,
+               new LaunchPadCustomPropertyOrderComparer(PropertySortOrder)
+            );
+        }
+
     }
 }
