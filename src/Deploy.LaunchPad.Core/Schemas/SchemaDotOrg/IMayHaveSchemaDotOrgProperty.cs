a2c882d7c514a60b4d9e88865b40342fbcffff0c
Charles Nicholas Kellett <11150209+NickKellett@users.noreply.github.com>
2024-12-28 10:51:39 -0500

    Add SchemaDotOrg interface

---
+++ src/Deploy.LaunchPad.Core/Schemas/SchemaDotOrg/IMayHaveSchemaDotOrgProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core.Schemas.SchemaDotOrg
+{
+    public partial interface IMayHaveSchemaDotOrgProperty<TSchema>
+       where TSchema : Schema.NET.IThing
+    {
+        public TSchema? SchemaDotOrg { get; }
+    }
+}
