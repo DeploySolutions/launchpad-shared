7c55062bb54ce1a31907e4ee09c9eeca4cfae219
Charles Nicholas Kellett <11150209+NickKellett@users.noreply.github.com>
2024-12-28 11:06:37 -0500

    Add ContentElementType enum

---
+++ src/Deploy.LaunchPad.Core/Content/ContentElementType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core.Content
+{
+
+    public enum ContentElementType
+    {
+        Article,
+        Report,
+        Topic,
+        Link,
+        Image,
+        Website,
+        WebPage,
+        Blog,
+        BlogPosting,
+        DataSet,
+        DataCatalog,
+        Video,
+        Event,
+        Podcast
+    }
+
+}
