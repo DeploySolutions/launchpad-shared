c3d32614b38bc8c9deeec9d112af461070d8e2b6
Charles Nicholas Kellett <11150209+NickKellett@users.noreply.github.com>
2024-12-28 11:06:25 -0500

    Add Content publishing element

---
+++ src/Deploy.LaunchPad.Core/Content/ILaunchPadContentPublishingElement.cs
@@ -0,0 +1,14 @@
+﻿using Deploy.LaunchPad.Core.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core.Content
+{
+    public partial interface ILaunchPadContentPublishingElement : ILaunchPadCommonProperties
+    {
+        ContentElementType Type { get; set; }
+    }
+}
