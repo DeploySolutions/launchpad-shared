546b9a2aa1551a19ece10cc39b7eebb00d7e99be
Charles Nicholas Kellett <me@nicholaskellett.com>
2025-01-02 09:52:41 -0500

    Add MDX file format

---
+++ src/Deploy.LaunchPad.Core/Files/Formats/IMdxFile.cs
@@ -0,0 +1,11 @@
+﻿using Deploy.LaunchPad.Core.Files.Formats;
+
+namespace Deploy.LaunchPad.Core.Files
+{
+    /// <summary>
+    /// MDX (Markdown for Components)
+    /// </summary>
+    public partial interface IMdxFile : IFile<string, MdxSchema>
+    {
+    }
+}
---
+++ src/Deploy.LaunchPad.Core/Files/Formats/MdxFile.cs
@@ -0,0 +1,25 @@
+﻿using Deploy.LaunchPad.Core.Files.Formats;
+using Deploy.LaunchPad.Util;
+using System;
+using System.Collections.Generic;
+
+namespace Deploy.LaunchPad.Core.Files
+{
+    /// <summary>
+    /// MDX (Markdown for Components)
+    /// </summary>
+    public partial class MdxFile : FileBase<string, MdxSchema>, IMdxFile
+    {
+
+        public override string Extension => ".mdx";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MdxFile"/> class.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        public MdxFile(string fileName) : base(fileName)
+        {
+
+        }
+    }
+}
