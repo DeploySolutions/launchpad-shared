fe18a28245147cdc5c33f167dab22d431bbd1df7
Charles Nicholas Kellett <me@nicholaskellett.com>
2025-05-06 13:13:03 -0400

    Add Overture Maps definition classes

---
+++ src/Deploy.LaunchPad.Core/Geospatial/OvertureMapsFoundation/OvertureMapsLocation.cs
@@ -0,0 +1,34 @@
+﻿using Deploy.LaunchPad.Core.Geospatial.Overture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core.Geospatial.OvertureMapsFoundation
+{
+    [Serializable]
+    public partial class OvertureMapsLocation : IMayHaveOvertureMapsLocation
+    {
+        public virtual Guid? GERSId { get; set; }
+        public virtual string Features { get; set; }
+
+        public OvertureMapsLocation()
+        {
+            GERSId = Guid.NewGuid();
+            Features = string.Empty;
+        }
+
+        public OvertureMapsLocation(Guid gersId)
+        {
+            GERSId = gersId;
+            Features = string.Empty;
+        }
+
+        public OvertureMapsLocation(Guid gersId, string features)
+        {
+            GERSId = gersId;
+            Features = features;
+        }
+    }
+}
