5c98656d461f28d097263d01c21f7f39fc0334a9
Charles Nicholas Kellett <me@nicholaskellett.com>
2025-05-16 10:37:10 -0400

    Add Id property marker interface

---
+++ src/Deploy.LaunchPad.Core/Domain/Model/IHaveAnIdProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Deploy.LaunchPad.Core.Domain.Model
+{
+    public partial interface IHaveAnIdProperty<TIdType>
+    {
+       // don't specify the actual property as if we inherit from ABP Domain Entity that will cause shadow property
+
+    }
+}
