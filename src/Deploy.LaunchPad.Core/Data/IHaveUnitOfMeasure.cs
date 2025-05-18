31a4e84187f70816b4536d376fb308653da1b938
Charles Nicholas Kellett <me@nicholaskellett.com>
2025-05-18 11:01:56 -0400

    Add unit of measure interface

---
+++ src/Deploy.LaunchPad.Core/Data/IHaveUnitOfMeasure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Deploy.LaunchPad.Core.Data
+{
+    public partial interface IHaveUnitOfMeasure
+    {
+        ///<summary>
+        /// Unit of measure (ex "kg", "m", "cm", "mm", "g", "l", "ml", "oz", "in", "ft", etc.)
+        ///</summary>
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public string UnitOfMeasure { get; set; }
+
+    }
+}
