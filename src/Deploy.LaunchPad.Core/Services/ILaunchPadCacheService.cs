fce338b17afcce0e6a578868e00d09bceda81a0e
Charles Nicholas Kellett <me@nicholaskellett.com>
2025-05-20 10:42:51 -0400

    Add basic cache service interface

---
+++ src/Deploy.LaunchPad.Core/Services/ILaunchPadCacheService.cs
@@ -0,0 +1,16 @@
+﻿using Deploy.LaunchPad.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core
+{
+    public partial interface ILaunchPadCacheService : ILaunchPadService
+    {
+        public Task ReloadAllAsync();
+
+        public Task<Boolean> InvalidateCacheAsync();
+    };
+}
