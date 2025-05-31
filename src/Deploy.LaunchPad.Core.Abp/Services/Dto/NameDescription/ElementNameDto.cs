be904614f77f769d31a2f675bc9ecf9175499cc6
Charles Nicholas Kellett <me@nicholaskellett.com>
2025-05-31 15:03:54 -0400

    Add missing short property to ElementName DTO

--- src/Deploy.LaunchPad.Core.Abp/Services/Dto/NameDescription/ElementNameDto.cs
+++ src/Deploy.LaunchPad.Core.Abp/Services/Dto/NameDescription/ElementNameDto.cs
@@ -1,8 +1,10 @@
 ﻿using Abp.AutoMapper;
+using Deploy.LaunchPad.Util.ValueConverters;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,7 @@ namespace Deploy.LaunchPad.Core.Services.Dto.NameDescription
 {
     [Serializable]
     [AutoMap(typeof(ElementName))]
-    public partial class ElementNameDto: ElementNameLightDto, IElementNameLightDto
+    public partial class ElementNameDto: ElementNameLightDto, IElementNameDto
     {
         [DataObjectField(false)]
         [XmlAttribute]
@@ -21,6 +23,35 @@ namespace Deploy.LaunchPad.Core.Services.Dto.NameDescription
         [DataObjectField(false)]
         [XmlAttribute]
         public virtual string Suffix { get; set; } = string.Empty;
+
+        protected string _short = string.Empty;        
+        /// <summary>
+        /// The short name of this element (if different from the FullName field). If not set, it will default to the first 50 characters of the full name.
+        /// </summary>
+        /// <value>The fully qualified name of the element.</value>
+        [MaxLength(50, ErrorMessageResourceName = "Validation_Name_Short_50CharsOrLess", ErrorMessageResourceType = typeof(Deploy_LaunchPad_Core_Resources))]
+        [DataObjectField(false)]
+        [XmlAttribute]
+        [JsonProperty("short", NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [JsonConverter(typeof(JsonEmptyStringToNullConverter))]
+        public virtual string Short
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_short))
+                {
+                    return Full;
+                }
+                else
+                {
+                    return _short;
+                }
+            }
+            set
+            {
+                _short = value;
+            }
+        }
 
     }
 }
---
+++ src/Deploy.LaunchPad.Core.Abp/Services/Dto/NameDescription/IElementNameDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace Deploy.LaunchPad.Core.Services.Dto.NameDescription
+{
+    public partial interface IElementNameDto: IElementNameLightDto
+    {
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public string Short { get; set; }
+
+
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public string Prefix { get; set; }
+
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public string Suffix { get; set; }
+    }
+}
