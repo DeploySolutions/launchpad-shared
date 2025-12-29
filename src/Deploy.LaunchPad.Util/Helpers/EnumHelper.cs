1c7313ccfb5c03c861d1241ae72a2bf26c40ef41
Charles Nicholas Kellett <nkellett@deploy.solutions>
2025-12-29 12:59:14 -0500

    Add EnumHelper

---
+++ src/Deploy.LaunchPad.Util/Helpers/EnumHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Deploy.LaunchPad.Util.Helpers
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var displayAttribute = enumValue.GetType()
+                .GetField(enumValue.ToString())
+                ?.GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.DisplayAttribute), false)
+                .FirstOrDefault() as System.ComponentModel.DataAnnotations.DisplayAttribute;
+
+            return displayAttribute?.Name ?? enumValue.ToString();
+        }
+    }
+}
