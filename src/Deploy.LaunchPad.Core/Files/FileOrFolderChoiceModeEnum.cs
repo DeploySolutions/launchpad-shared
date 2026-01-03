e58bde492c64ac1c20c3663046471aa6e4545352
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-01-03 17:11:49 -0500

    Add FileorFolderChoiceEnum

---
+++ src/Deploy.LaunchPad.Core/Files/FileOrFolderChoiceModeEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Deploy.LaunchPad.Core.Files
+{
+    /// <summary>
+    /// Allows a choice between manipulating files or folders.
+    /// </summary>
+    public enum FileOrFolderChoiceMode
+    {
+        [Description("Files")]
+        Files,
+        [Description("Folders")]
+        Folders,
+        [Description("Files and Folders")]
+        FilesAndFolders
+    }
+}
