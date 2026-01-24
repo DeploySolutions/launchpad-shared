1bf36e9ccc6a8d7d4a94d58be74296a940c7084d
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-01-24 15:31:28 -0500

    Add MinimalProperties

---
+++ src/Deploy.LaunchPad.Util/Elements/ILaunchPadMinimalProperties.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Deploy.LaunchPad.Util.Elements
+{
+    public partial interface ILaunchPadMinimalProperties
+    {
+
+        /// <summary>
+        /// The name of this object
+        /// </summary>
+        /// <value>The name.</value>
+        [Required]
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public ElementName Name { get; set; }
+
+        /// <summary>
+        /// The description of this object
+        /// </summary>
+        /// <value>The description.</value>
+        [DataObjectField(false)]
+        [XmlAttribute]
+        public ElementDescription Description { get; set; }
+
+    }
+}
