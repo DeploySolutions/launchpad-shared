4f0e0e2add8158ac83b4dce8f7fd6fe22761990a
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-02-15 12:18:44 -0500

    Add warning classes

---
+++ src/Deploy.LaunchPad.Core/Results/IWarning.cs
@@ -0,0 +1,18 @@
+﻿using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core.Results
+{
+
+    public partial interface IWarning : ISuccess
+    {
+        //
+        // Summary:
+        //     Reasons of the error
+        public List<IWarning> Reasons { get; }
+    }
+}
---
+++ src/Deploy.LaunchPad.Core/Results/Warning.cs
@@ -0,0 +1,26 @@
+﻿using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deploy.LaunchPad.Core.Results
+{
+    [Serializable]
+    public partial class Warning : Success, IWarning
+    {
+        //
+        // Summary:
+        //     Reasons of the error
+        public List<IWarning> Reasons { get; }
+
+        protected Warning() : base()
+        {
+        }
+
+        public Warning(string message) : base(message)
+        {
+        }
+    }
+}
