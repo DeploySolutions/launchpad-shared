66b4912cd93a9c6acdd87f6648aa6db1338ec00c
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-02-19 09:31:26 -0500

    Updating licenses

--- src/Deploy.LaunchPad.Util/Extensions/ExceptionExtensions.cs
+++ src/Deploy.LaunchPad.Util/Extensions/ExceptionExtensions.cs
@@ -1,29 +1,34 @@
-// SPDX-License-Identifier: Apache-2.0
-//
-// This file contains code originally from the ASP.NET Boilerplate - Web Application Framework project:
-//   Repository: https://github.com/aspnetboilerplate/aspnetboilerplate
-//   Original license: MIT License
-//
-// Modifications in this forked/copied version:
-//   - Integrated into Deploy.LaunchPad.X (namespace adjustments)
-//   - Local fixes and adaptations (see git history for details)
-//
-// Original Copyright (c) Volosoft (https://volosoft.com) and contributors
-// Modified files Copyright (c) Deploy Software Solutions, 2026
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// [EO License Header]
-
+#region "Licensing"
+/*
+ * SPDX-FileCopyrightText: Copyright (c) Volosoft (https://volosoft.com) and contributors
+ * SPDX-License-Identifier: MIT
+ *
+ * This file contains code originally from the ASP.NET Boilerplate - Web Application Framework:
+ *   Repository: https://github.com/aspnetboilerplate/aspnetboilerplate
+ *
+ * The original portions of this file remain licensed under the MIT License.
+ * You may obtain a copy of the MIT License at:
+ *
+ *   https://opensource.org/license/mit
+ *
+ *
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Deploy Software Solutions (https://www.deploy.solutions)
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Modifications and additional code in this file are licensed under
+ * the Apache License, Version 2.0.
+ *
+ * You may obtain a copy of the Apache License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Apache License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the applicable license for governing permissions and limitations.
+ */
+#endregion
 
 using System;
 using System.Runtime.ExceptionServices;
