f09de46766be3a9cb6595e3df240aa527f61878e
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-02-22 13:57:54 -0500

    Migrate File helpers and object mappers

---
+++ src/Deploy.LaunchPad.Util/Helpers/DirectoryHelper.cs
@@ -0,0 +1,67 @@
+#region "Licensing"
+/*
+ * SPDX-FileCopyrightText: Copyright (c) Volosoft (https://volosoft.com) and contributors
+ * SPDX-License-Identifier: MIT
+ *
+ * This file contains code originally from the ASP.NET Boilerplate - Web Application Framework:
+ *   Repository: https://github.com/aspnetboilerplate/aspnetboilerplate
+ *
+ * The original portions of this file remain licensed under the MIT License.
+ * You may obtain a copy of the MIT License at:
+ *
+ *   https://opensource.org/license/mit
+ *
+ *
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Deploy Software Solutions (https://www.deploy.solutions)
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Modifications and additional code in this file are licensed under
+ * the Apache License, Version 2.0.
+ *
+ * You may obtain a copy of the Apache License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Apache License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the applicable license for governing permissions and limitations.
+ */
+#endregion
+
+using System.IO;
+
+namespace Deploy.LaunchPad.Util.Helpers
+{
+    /// <summary>
+    /// A helper class for Directory operations.
+    /// </summary>
+    public static class DirectoryHelper
+    {
+        /// <summary>
+        /// Creates a new directory if it does not exists.
+        /// </summary>
+        /// <param name="directory">Directory to create</param>
+        public static void CreateIfNotExists(string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+
+        /// <summary>
+        /// Delete directory if exists.
+        /// </summary>
+        /// <param name="directory">Directory to create</param>
+        /// <param name="recursive">Delete sub-directory</param>
+        public static void DeleteIfExists(string directory, bool recursive)
+        {
+            if (Directory.Exists(directory))
+            {
+                Directory.Delete(directory, recursive);
+            }
+        }
+    }
+}
---
+++ src/Deploy.LaunchPad.Util/ObjectMapping/IObjectMapper.cs
@@ -0,0 +1,68 @@
+#region "Licensing"
+/*
+ * SPDX-FileCopyrightText: Copyright (c) Volosoft (https://volosoft.com) and contributors
+ * SPDX-License-Identifier: MIT
+ *
+ * This file contains code originally from the ASP.NET Boilerplate - Web Application Framework:
+ *   Repository: https://github.com/aspnetboilerplate/aspnetboilerplate
+ *
+ * The original portions of this file remain licensed under the MIT License.
+ * You may obtain a copy of the MIT License at:
+ *
+ *   https://opensource.org/license/mit
+ *
+ *
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Deploy Software Solutions (https://www.deploy.solutions)
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Modifications and additional code in this file are licensed under
+ * the Apache License, Version 2.0.
+ *
+ * You may obtain a copy of the Apache License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Apache License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the applicable license for governing permissions and limitations.
+ */
+#endregion
+
+using System.Linq;
+
+namespace Deploy.LaunchPad.Util.ObjectMapping
+{
+    /// <summary>
+    /// Defines a simple interface to map objects.
+    /// </summary>
+    public interface IObjectMapper
+    {
+        /// <summary>
+        /// Converts an object to another. Creates a new object of <typeparamref name="TDestination"/>.
+        /// </summary>
+        /// <typeparam name="TDestination">Type of the destination object</typeparam>
+        /// <param name="source">Source object</param>
+        TDestination Map<TDestination>(object source);
+
+        /// <summary>
+        /// Execute a mapping from the source object to the existing destination object
+        /// </summary>
+        /// <typeparam name="TSource">Source type</typeparam>
+        /// <typeparam name="TDestination">Destination type</typeparam>
+        /// <param name="source">Source object</param>
+        /// <param name="destination">Destination object</param>
+        /// <returns>Returns the same <paramref name="destination"/> object after mapping operation</returns>
+        TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
+
+        /// <summary>
+        /// Project the input queryable.
+        /// </summary>
+        /// <remarks>Projections are only calculated once and cached</remarks>
+        /// <typeparam name="TDestination">Destination type</typeparam>
+        /// <param name="source">Queryable source</param>
+        /// <returns>Queryable result, use queryable extension methods to project and execute result</returns>
+        IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source);
+    }
+}
