9ebd249fa32a022795ef57f9c20d92964e9e9352
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-02-22 13:47:52 -0500

    Migrate Linq classes into Core

---
+++ src/Deploy.LaunchPad.Core/Linq/Expressions/ExpressionCombiner.cs
@@ -0,0 +1,90 @@
+#region "Licensing"
+/*
+ * SPDX-FileCopyrightText: Copyright (c) Volosoft (https://volosoft.com) and contributors
+ * SPDX-License-Identifier: MIT
+ *
+ * This file contains code originally from the ASP.NET Boilerplate - Web Application Framework:
+ *   Repository: https://github.com/aspnetboilerplate/aspnetboilerplate
+ *
+ * The original portions of this file remain licensed under the MIT License.
+ * You may obtain a copy of the MIT License at:
+ *
+ *   https://opensource.org/license/mit
+ *
+ *
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Deploy Software Solutions (https://www.deploy.solutions)
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Modifications and additional code in this file are licensed under
+ * the Apache License, Version 2.0.
+ *
+ * You may obtain a copy of the Apache License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Apache License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the applicable license for governing permissions and limitations.
+ */
+#endregion
+
+using System;
+using System.Linq.Expressions;
+
+namespace Deploy.LaunchPad.Core.Linq.Expressions
+{
+    public static class ExpressionCombiner
+    {
+        public static Expression<Func<T, bool>> Combine<T>(Expression<Func<T, bool>> expression1, Expression<Func<T, bool>> expression2)
+        {
+            if (expression1 == null && expression2 == null)
+            {
+                return null;
+            }
+
+            if (expression1 == null)
+            {
+                return expression2;
+            }
+
+            if (expression2 == null)
+            {
+                return expression1;
+            }
+
+            var parameter = Expression.Parameter(typeof(T));
+
+            var leftVisitor = new ReplaceExpressionVisitor(expression1.Parameters[0], parameter);
+            var left = leftVisitor.Visit(expression1.Body);
+
+            var rightVisitor = new ReplaceExpressionVisitor(expression2.Parameters[0], parameter);
+            var right = rightVisitor.Visit(expression2.Body);
+
+            return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(left, right), parameter);
+        }
+
+        class ReplaceExpressionVisitor : ExpressionVisitor
+        {
+            private readonly Expression _oldValue;
+            private readonly Expression _newValue;
+
+            public ReplaceExpressionVisitor(Expression oldValue, Expression newValue)
+            {
+                _oldValue = oldValue;
+                _newValue = newValue;
+            }
+
+            public override Expression Visit(Expression node)
+            {
+                if (node == _oldValue)
+                {
+                    return _newValue;
+                }
+
+                return base.Visit(node);
+            }
+        }
+    }
+}
---
+++ src/Deploy.LaunchPad.Core/Linq/Extensions/QueryableExtensions.cs
@@ -0,0 +1,96 @@
+#region "Licensing"
+/*
+ * SPDX-FileCopyrightText: Copyright (c) Volosoft (https://volosoft.com) and contributors
+ * SPDX-License-Identifier: MIT
+ *
+ * This file contains code originally from the ASP.NET Boilerplate - Web Application Framework:
+ *   Repository: https://github.com/aspnetboilerplate/aspnetboilerplate
+ *
+ * The original portions of this file remain licensed under the MIT License.
+ * You may obtain a copy of the MIT License at:
+ *
+ *   https://opensource.org/license/mit
+ *
+ *
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Deploy Software Solutions (https://www.deploy.solutions)
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Modifications and additional code in this file are licensed under
+ * the Apache License, Version 2.0.
+ *
+ * You may obtain a copy of the Apache License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Apache License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the applicable license for governing permissions and limitations.
+ */
+#endregion
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Deploy.LaunchPad.Core.Application.Services.Dto;
+
+namespace Deploy.LaunchPad.Core.Linq.Extensions
+{
+    /// <summary>
+    /// Some useful extension methods for <see cref="IQueryable{T}"/>.
+    /// </summary>
+    public static class QueryableExtensions
+    {
+        /// <summary>
+        /// Used for paging. Can be used as an alternative to Skip(...).Take(...) chaining.
+        /// </summary>
+        public static IQueryable<T> PageBy<T>(this IQueryable<T> query, int skipCount, int maxResultCount)
+        {
+            if (query == null)
+            {
+                throw new ArgumentNullException("query");
+            }
+
+            return query.Skip(skipCount).Take(maxResultCount);
+        }
+
+        /// <summary>
+        /// Used for paging with an <see cref="IPagedResultRequest"/> object.
+        /// </summary>
+        /// <param name="query">Queryable to apply paging</param>
+        /// <param name="pagedResultRequest">An object implements <see cref="IPagedResultRequest"/> interface</param>
+        public static IQueryable<T> PageBy<T>(this IQueryable<T> query, IPagedResultRequest pagedResultRequest)
+        {
+            return query.PageBy(pagedResultRequest.SkipCount, pagedResultRequest.MaxResultCount);
+        }
+
+        /// <summary>
+        /// Filters a <see cref="IQueryable{T}"/> by given predicate if given condition is true.
+        /// </summary>
+        /// <param name="query">Queryable to apply filtering</param>
+        /// <param name="condition">A boolean value</param>
+        /// <param name="predicate">Predicate to filter the query</param>
+        /// <returns>Filtered or not filtered query based on <paramref name="condition"/></returns>
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            return condition
+                ? query.Where(predicate)
+                : query;
+        }
+
+        /// <summary>
+        /// Filters a <see cref="IQueryable{T}"/> by given predicate if given condition is true.
+        /// </summary>
+        /// <param name="query">Queryable to apply filtering</param>
+        /// <param name="condition">A boolean value</param>
+        /// <param name="predicate">Predicate to filter the query</param>
+        /// <returns>Filtered or not filtered query based on <paramref name="condition"/></returns>
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, int, bool>> predicate)
+        {
+            return condition
+                ? query.Where(predicate)
+                : query;
+        }
+    }
+}
