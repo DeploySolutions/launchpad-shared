149d1b30c3c0392f08e14f6b65d19da706cf0b30
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-02-22 18:59:20 -0500

    Migrate DisposeAction

---
+++ src/Deploy.LaunchPad.Util/DisposeAction.cs
@@ -0,0 +1,65 @@
+#region "Licensing"
+/*
+ * SPDX-FileCopyrightText: Copyright (c) Volosoft (https://volosoft.com) and contributors
+ * SPDX-License-Identifier: MIT
+ *
+ * This file contains code originally from the ASP.NET Boilerplate - Web Application Framework:
+ *   Repository: https://github.com/aspnetboilerplate/aspnetboilerplate
+ *
+ * The original portions of this file remain licensed under the MIT License.
+ * You may obtain a copy of the MIT License at:
+ *
+ *   https://opensource.org/license/mit
+ *
+ *
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Deploy Software Solutions (https://www.deploy.solutions)
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Modifications and additional code in this file are licensed under
+ * the Apache License, Version 2.0.
+ *
+ * You may obtain a copy of the Apache License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Apache License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the applicable license for governing permissions and limitations.
+ */
+#endregion
+
+using System;
+using System.Threading;
+using JetBrains.Annotations;
+
+namespace LaunchPad.Core.Util
+{
+    /// <summary>
+    /// This class can be used to provide an action when
+    /// Dipose method is called.
+    /// </summary>
+    public class DisposeAction : IDisposable
+    {
+        public static readonly DisposeAction Empty = new DisposeAction(null);
+
+        private Action _action;
+
+        /// <summary>
+        /// Creates a new <see cref="DisposeAction"/> object.
+        /// </summary>
+        /// <param name="action">Action to be executed when this object is disposed.</param>
+        public DisposeAction([CanBeNull] Action action)
+        {
+            _action = action;
+        }
+
+        public void Dispose()
+        {
+            // Interlocked prevents multiple execution of the _action.
+            var action = Interlocked.Exchange(ref _action, null);
+            action?.Invoke();
+        }
+    }
+}
