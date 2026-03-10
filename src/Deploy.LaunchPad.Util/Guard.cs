73c4c4ce91eac2824a3a94368f8d52782601903d
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-03-10 09:28:41 -0400

    Rename Guard methods for clarity

--- src/Deploy.LaunchPad.Util/Guard.cs
+++ src/Deploy.LaunchPad.Util/Guard.cs
@@ -41,7 +41,7 @@ namespace Deploy.LaunchPad.Util
     public static partial class Guard
     {
         [ContractAnnotation("value:null => halt")]
-        public static T AgainstNotNull<T>(T value, [InvokerParameterName][NotNull] string parameterName)
+        public static T AgainstNull<T>(T value, [InvokerParameterName][NotNull] string parameterName)
         {
             if (value == null)
             {
@@ -52,18 +52,18 @@ namespace Deploy.LaunchPad.Util
         }
 
         [ContractAnnotation("value:null => halt")]
-        public static string AgainstNotNullOrEmpty(string value, [InvokerParameterName][NotNull] string parameterName)
+        public static string AgainstNullOrEmpty(string value, [InvokerParameterName][NotNull] string parameterName)
         {
             if (value.IsNullOrEmpty())
             {
-                throw new ArgumentException($"{parameterName} can not be null or empty!", parameterName);
+                throw new ArgumentNullException($"{parameterName} can not be null or empty!", parameterName);
             }
 
             return value;
         }
 
         [ContractAnnotation("value:null => halt")]
-        public static string AgainstNotNullOrWhiteSpace(string value, [InvokerParameterName][NotNull] string parameterName)
+        public static string AgainstNullOrWhiteSpace(string value, [InvokerParameterName][NotNull] string parameterName)
         {
             if (value.IsNullOrWhiteSpace())
             {
@@ -74,11 +74,11 @@ namespace Deploy.LaunchPad.Util
         }
 
         [ContractAnnotation("value:null => halt")]
-        public static ICollection<T> AgainstNotNullOrEmpty<T>(ICollection<T> value, [InvokerParameterName][NotNull] string parameterName)
+        public static ICollection<T> AgainstNullOrEmpty<T>(ICollection<T> value, [InvokerParameterName][NotNull] string parameterName)
         {
             if (value.IsNullOrEmpty())
             {
-                throw new ArgumentException(parameterName + " can not be null or empty!", parameterName);
+                throw new ArgumentNullException(parameterName + " can not be null or empty!", parameterName);
             }
 
             return value;
