9b8283f61a128b1a0fd0cb91f9ee0f28cc8b58b7
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-03-10 12:29:21 -0400

    Create IMustHaveDisplayName.cs

---
+++ src/Deploy.LaunchPad.Core/Metadata/IMustHaveDisplayName.cs
@@ -0,0 +1,10 @@
+﻿using Deploy.LaunchPad.Core.Localization;
+using System;
+
+namespace Deploy.LaunchPad.Core.Metadata
+{
+    public partial interface IMustHaveDisplayName
+    {
+        public ILocalizableString DisplayName { get; }
+    }
+}
