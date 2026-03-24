38dbc866a3c938ac32f0da79894561b4b4dd28ce
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-03-24 16:34:56 -0400

    Add Thumbs Up or Down rating for user response / model training

---
+++ src/Deploy.LaunchPad.Core/Qualifiers/ThumbsUpRating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Deploy.LaunchPad.Core.Qualifiers
+{
+    public enum ThumbRating
+    {
+        [Description("None")]
+        None = 0,
+        [Description("Thumbs Up")]
+        ThumbsUp = 1,
+        [Description("Thumbs Down")]
+        ThumbsDown = 2
+    }
+}
