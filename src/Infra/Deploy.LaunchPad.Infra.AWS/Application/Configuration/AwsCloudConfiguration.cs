703a633a564f0a06d026b778b35f285efee29f41
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-04-04 15:37:15 -0400

    Add an AwsCloudConfiguration setting

---
+++ src/Infra/Deploy.LaunchPad.Infra.AWS/Application/Configuration/AwsCloudConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Deploy.LaunchPad.Infra.Aws;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deploy.LaunchPad.Infra.AWS.Application.Configuration
+{
+    public partial class AwsCloudConfiguration
+    {
+        public virtual string RegionEndpointName { get; set; } = Constants_LaunchPadInfraAWS.DefaultRegionEndpointName;
+
+        public virtual AwsCloudConfigurationCredentials Credentials { get; set; } 
+
+        public AwsCloudConfiguration() 
+        {
+            Credentials = new AwsCloudConfigurationCredentials();   
+        }
+    }
+
+    public partial class AwsCloudConfigurationCredentials
+    {
+       
+        public virtual string LocalProfileName { get; set; } = Constants_LaunchPadInfraAWS.DefaultLocalProfileName;
+
+        public virtual bool ShouldUseLocalProfile { get; set; } = false;
+
+        public AwsCloudConfigurationCredentials() { }
+    }
+}
