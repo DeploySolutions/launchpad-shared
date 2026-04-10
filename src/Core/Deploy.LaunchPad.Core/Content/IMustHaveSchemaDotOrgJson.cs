16e61cbba28fec1cd45675c8bfb4d4920224497f
Charles Nicholas Kellett <nkellett@deploy.solutions>
2026-04-10 08:08:58 -0400

    Refactor SchemaDotOrg interface

--- src/Core/Deploy.LaunchPad.Core/Content/IMustHaveSchemaDotOrgJson.cs
+++ src/Core/Deploy.LaunchPad.Core/Content/IMustHaveSchemaDotOrgJson.cs
@@ -1,20 +1,20 @@
-﻿using Newtonsoft.Json;
+﻿using Deploy.LaunchPad.Util.Metadata;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Deploy.LaunchPad.Util.Metadata
+namespace Deploy.LaunchPad.Core.Content
 {
-    public partial interface IMayHaveSchemaDotOrgJson
+    public partial interface IMustHaveSchemaDotOrgJson<TSchemaDotOrg>
     {
+        public TSchemaDotOrg? SchemaType { get; set; }
 
         /// <summary>
         /// Gets or sets the schema.
         /// </summary>
         /// <value>The schema.</value>
         public string? SchemaDotOrgJson { get; set; }
-
-        public string? EntityType { get; set; }
 
         public ILaunchPadSchemaDetails<JsonToken>? SchemaDetails { get; set; }
     }
